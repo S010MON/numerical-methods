cc2bd32c075589d0ccea550a128ebff17aeaa6c3
Leon <10490509+S010MON@users.noreply.github.com>
2021-04-09 17:29:18 +0200

    Add files via upload

---
+++ Calc/euler_solver.m
@@ -0,0 +1,13 @@
+function [y] = euler_solver(f,t0,tn,y,h)
+%EULER METHOD
+%   Give a differential equation f(t,y)generates an approximation
+%   of the solution by stepping from t0 to tn in increments of h
+%   at each step it ypdates the new y value before using it for the next
+%   calculation
+
+    while t0 < tn               % While the current step is smaller than the final
+        y = y + f(t0,y)*h;      % Calculate the difference in y by adding f(t,y)*h  
+        t0 = t0+h;              % Increment the step counter
+    end
+end
+
