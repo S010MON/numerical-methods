87f930a3fa86cc516c4bec9a5c60632f31dd9899
S010MON <leon@bdebnath.com>
2021-11-19 16:39:34 +0100

    e values A and B completed

--- DataAndTemplates/myevalueA.m
+++ DataAndTemplates/myevalueA.m
@@ -2,4 +2,30 @@
 % 
 %  Assignment 2 | Prof. Alberto Paccanaro
 % 
-% INSERT YOUR CODE HERE!
+function e_hat = myevalueA(err)
+
+	% Set the value for 'n' in our formula to 1
+	n = 1;
+
+	% Set the first estimate to a stupidly hight number
+	e_hat = 100000;
+
+	% Calculate the error 'epsilon' between the estimate and the actual value 
+	epsilon = abs(exp(1) - e_hat);
+
+	% While the error is greater than the inputted 'err' value
+	while epsilon > err
+
+		% Use the given formula to find the inverse
+		e_inverse = (1 - (1/n))^n;
+
+		% Invert to get an estimated value for e
+		e_hat = 1/e_inverse;
+
+		% recaluculate the new error (for the while loop to test)
+		epsilon = abs(exp(1) - e_hat);
+
+		% increment the counter
+		n = n+1;
+	end %while
+end %function
--- DataAndTemplates/myevalueB.m
+++ DataAndTemplates/myevalueB.m
@@ -2,4 +2,28 @@
 % 
 %  Assignment 2 | Prof. Alberto Paccanaro
 % 
-% INSERT YOUR CODE HERE!
+function e_hat = myevalueB(err)
+
+	% Set the value for 'n' in our formula to 1
+	n = 0;
+
+	% Set the first estimate to a stupidly hight number
+	e_hat = 0;
+
+	% Calculate the error 'epsilon' between the estimate and the actual value 
+	epsilon = abs(exp(1) - e_hat);
+
+	% While the error is greater than the inputted 'err' value
+	while epsilon > err
+
+			% Use the given formula to find the inverse
+		e_hat = e_hat + (1/factorial(n))
+
+		% recaluculate the new error (for the while loop to test)
+		epsilon = abs(exp(1) - e_hat)
+	
+		% increment the counter
+		n = n+1;
+
+	end %while
+end %function
