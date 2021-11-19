d65d05baf3888f3dcab15e5a9d7263ee4996c572
S010MON <leon@bdebnath.com>
2021-11-19 10:09:37 +0100

    adds all files

---
+++ DataAndTemplates/caloriesaverage.m
@@ -0,0 +1,10 @@
+% CS5810  -- Programming for data analysis 
+%
+%  Assignment 2 | Prof. Alberto Paccanaro
+% 
+function ave_calories = caloriesaverage(cal)
+	if cal = 0
+		ave_calories = 0;
+	else
+		
+			
---
+++ DataAndTemplates/displaycarmodels.m
@@ -0,0 +1,5 @@
+% CS5810  -- Programming for data analysis 
+%
+%  Assignment 2 | Prof. Alberto Paccanaro
+% 
+% INSERT YOUR CODE HERE !
