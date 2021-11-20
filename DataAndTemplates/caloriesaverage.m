984f40783362e1c1ec047bc7eb8ee3d597b31a97
S010MON <leon@bdebnath.com>
2021-11-20 10:51:45 +0100

    caloriesaverage.m elseif statement removed

--- DataAndTemplates/caloriesaverage.m
+++ DataAndTemplates/caloriesaverage.m
@@ -14,17 +14,14 @@ function ave_calories = caloriesaverage(cal)
 		total = 0;
 		count = 0;
 		ave_calories = 0;
-	
-	% Case 2: we recive our first input, so we create our variables
-	else if isempty(count)
-		count = 1;
-		total = cal;
-		ave_calories = total / count;
 		
-	% Case 3: anything else, we add to the total and divide by (count + 1)
+	% Case 2: anything else, we add to the total and divide by (count + 1)
 	else
 		total = total + cal;
 		count = count + 1;
 		ave_calories = total / count;
-	end 
-end	
+		if(total > 2000)
+			fprintf('You have reached your daily recommended maximum calories\n');
+		end %if
+	end %if
+end %function
--- DataAndTemplates/ecompare.m
+++ DataAndTemplates/ecompare.m
@@ -1,5 +1,27 @@
 % CS5810  -- Programming for data analysis 
 %
 %  Assignment 2 | Prof. Alberto Paccanaro
-%  
-% INSERT YOUR CODE HERE!
+%
+function output = ecompare(X)
+	
+	Y_error_a = zeros(size(X));
+       	Y_iter_a  = zeros(size(X));
+       	Y_error_b = zeros(size(X));
+	Y_iter_b  = zeros(size(X));
+
+	for i = 1:length(X)
+		Y_a = myevalueA(X(i));
+		Y_error_a(i) = Y_a(1);
+		Y_iter_a(i) = Y_a(2);
+
+		Y_b = myevalueB(X(i));
+		Y_error_b(i) = Y_b(1);
+		Y_iter_b(i) = Y_b(2);
+	end %for
+
+	output = [X; Y_error_a; Y_error_b];
+	scatter(X, Y_error_a, 'filled')
+	hold on
+	scatter(X, Y_iter
+
+end %function
