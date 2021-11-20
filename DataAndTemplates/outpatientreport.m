2f7ef91b5eadb0a6383e97a3bcf30263503b2b5b
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2021-11-20 17:06:42 +0100

    Update outpatientreport.m

--- DataAndTemplates/outpatientreport.m
+++ DataAndTemplates/outpatientreport.m
@@ -2,4 +2,30 @@
 % 
 %  Assignment 2 | Prof. Alberto Paccanaro
 % 
-% INSERT YOUR CODE HERE!
+function [data] = outpatientreport()
+
+    % Load data
+    data = load('myhospital.mat');
+    
+    % Print out 10 random details
+    for i = 1:10
+    
+        % Generate a random integer
+        r = randi(length(data.LastName));
+        
+        % Index all variables for the random 'r'
+        name = string(data.LastName(r));
+        sex = string(data.Sex(r));
+        age = data.Age(r);
+        weight = data.Weight(r);
+        smoker = data.isSmoker(r);
+        
+        % Print out to terminal
+        formatspec = '%s %s %i %i %i\n'; 
+        fprintf(formatspec, name, sex, age, weight, smoker);
+    end %for
+        
+    % Plot weight against smoking 
+    figure
+    boxplot(data.Weight, data.isSmoker);
+end %function
