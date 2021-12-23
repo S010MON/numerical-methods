1c8d38cd98cc82b5e600f3f16679bed14421177a
S010MON <leon@bdebnath.com>
2021-12-23 15:51:32 +0000

    add label updates

--- k_means/k_means.m
+++ k_means/k_means.m
@@ -25,46 +25,29 @@ hold off
 
 
 %% Iterate through the data and, for each element, assign the label to the closest centroid
-for i = 1:length(data)
-	min_dist = 1000000;
-	min_label = 0;
-	x_1 = data(i,1);
-	y_1 = data(i,2);
+%for i = 1:length(data)
+%	min_dist = 1000000;
+%	min_label = 0;
+%	x_1 = data(i,1);
+%	y_1 = data(i,2);
+%
+%	for j = 1:length(centroids)
+%		y_2 = centroids(j,2);
+%		x_2 = centroids(j,1);
+%		d = dist(x_1, y_1, x_2, y_2);
+%		if d < min_dist
+%			min_dist = d;
+%		 	min_label = j;		
+%		end%if
+%	end%for
+%	
+%	data(i,3) = min_label;
+%end%for
 
-	for j = 1:length(centroids)
-		y_2 = centroids(j,2);
-		x_2 = centroids(j,1);
-		d = dist(x_1, y_1, x_2, y_2);
-		if d < min_dist
-			min_dist = d;
-		 	min_label = j;		
-		end%if
-	end%for
-	
-	data(i,3) = min_label;
-end%for
+R = updateLabels(centroids, data);
+centroids = R{1};
+data = R{2};
 
-data
+plotKmeans(centroids, data);
 
-%% Set the colours for the scatter plot
-c = [1 0 0   #red     (index 1)
-     0 1 0   #green   (index 2)
-     0 0 1   #blue    (index 3)
-     1 1 0   #yellow  (index 4)
-     1 0 1]; #magenta (index 5)
-
-scatter(centroids(:,1), centroids(:,2), "r", "filled");  
-hold on
-for i = 1:length(data)
-	if data(i,3) == 1
-		scatter(data(i,1), data(i,2), "r")
-	end
-	if data(i,3) == 2
-		scatter(data(i,1), data(i,2), "g")
-	end
-	if data(i,3) == 3
-		scatter(data(i,1), data(i,2), "b")
-	end%if
-end%for
-hold off
 
