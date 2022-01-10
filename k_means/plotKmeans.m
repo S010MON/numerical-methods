2c0a5509a194c34bfb05f338b4ea76c55011102b
S010MON <leon@bdebnath.com>
2022-01-10 12:00:14 +0000

    Adds 3d plotting

--- k_means/plotKmeans.m
+++ k_means/plotKmeans.m
@@ -1,16 +1,39 @@
 function out = plotKmeans(centroids, data, labels)
 
-	scatter(centroids(:,1), centroids(:,2), "k", "filled");
-	hold on
-	for i = 1:length(data)
-		if labels(i) == 1
-			scatter(data(i,1), data(i,2), "r")
-		elseif labels(i) == 2
-			scatter(data(i,1), data(i,2), "g")
-		elseif labels(i) == 3
-			scatter(data(i,1), data(i,2), "b")
-		end%if
-	end%for
-	hold off
+	% See how many dimenstions are in the data
+	dims = size(data)(2);
+	
+	if(dims == 2)
+		scatter(centroids(:,1), centroids(:,2), "k", "filled");
+		hold on
+		for i = 1:length(data)
+			if labels(i) == 1
+				scatter(data(i,1), data(i,2), "r")
+			elseif labels(i) == 2
+				scatter(data(i,1), data(i,2), "g")
+			elseif labels(i) == 3
+				scatter(data(i,1), data(i,2), "b")
+			elseif labels(i) == 4
+				scatter(data(i,1), data(i,2), "y")
+			end%if
+		end%for
+		hold off
+
+	elseif(dims == 3)
+		scatter3(centroids(:,1), centroids(:,2), centroids(:,3), "k", "filled");
+		hold on
+		for i = 1:length(data)
+			if labels(i) == 1
+				scatter(data(i,1), data(i,2), data(i,3),"r")
+			elseif labels(i) == 2
+				scatter(data(i,1), data(i,2), data(i,3),"g")
+			elseif labels(i) == 3
+				scatter(data(i,1), data(i,2), data(i,3),"b")
+			elseif labels(i) == 4
+				scatter(data(i,1), data(i,2), data(i,3),"y")
+			end%if
+		end%for
+		hold off
+	end%if
 
 end%function
